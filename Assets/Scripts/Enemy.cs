1b6fb845153992b9b9c76d2efd1802b45cd338cc
IyadAlanssary <77411974+IyadAlanssary@users.noreply.github.com>
2022-01-23 00:02:10 +0200

    Add Enemy chase and player health

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+{
+    [Header ("Movement")]
+    public float speed = 3f;
+    
+    [Header ("Attack")]
+    [SerializeField] private float attackDamage = 10f;
+    [SerializeField] private float attackSpeed = 1f;
+    private float canAttack;
+
+    [Header ("Health")]
+    [SerializeField] private float maxHealth;
+    private float Health;
+    private Transform target;
+    
+
+    void Start()
+    {
+        Health = maxHealth;
+    }
+    public void TakeDamage(float dmg) {
+        Health -= dmg;
+        Debug.Log("Enemy Health: "+ Health);
+        
+
+        if(Health <= 0){
+            Destroy(gameObject);
+            }
+        }
+
+    void FixedUpdate(){
+        if (target != null){
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+        }           
+    }
+    
+    
+    void OnTriggerEnter2D (Collider2D other){
+        if (other.gameObject.tag == "Player"){
+            target = other.transform;
+        }
+    }
+    void OnTriggerExit2D (Collider2D other){
+        if (other.gameObject.tag == "Player"){
+            target = null;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)    {
+        if (other.gameObject.tag == "Player"){
+            if(attackSpeed <= canAttack){
+                other.gameObject.GetComponent<PlayerHealth>().UpdateHealth(-attackDamage);
+                canAttack = 0f;
+            }
+            else{
+                canAttack += Time.deltaTime;
+            }
+        }
+}
+
+
+    private void OnCollisionStay2D(Collision2D other)    {
+        if (other.gameObject.tag == "Player"){
+            if(attackSpeed <= canAttack){
+                
+                other.gameObject.GetComponent<PlayerHealth>().UpdateHealth(-attackDamage);
+                canAttack = 0f;
+            }
+            else{
+                canAttack += Time.deltaTime;
+            }
+        }
+    }
+    
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    
+    private float health = 0f;
+    [SerializeField] private float maxHealth = 100f;
+    //[SerializeField] private Slider helathSlider;
+
+    private void Start()
+    {
+        health = maxHealth;
+        //helathSlider.maxValue = maxHealth;
+    }
+
+    public void UpdateHealth(float mod){
+        health += mod;
+        Debug.Log("Player Health: " + health);
+        if(health>maxHealth)
+            health=maxHealth;
+        else if (health<=0f){
+            health = 0f;
+            Debug.Log("Player Respawn");
+            //helathSlider.value = health;
+            PlayerDied();
+         }
+    }
+
+    private void PlayerDied(){
+        //LevelManager.instance.gameOver();
+        Destroy(gameObject);
+        //gameObject.SetActive(false);
+    }
+    // private void OnGUI() {
+    //     float t= Time.deltaTime / 1f;
+    //     helathSlider.value = Mathf.Lerp(helathSlider.value, health, t);
+    // }
+
+}
